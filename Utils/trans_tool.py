6a32da41122385823b521fb18f752526e30751de
Orange-66 <qity_66@outlook.com>
2021-02-03 20:49:38 +0800

    - 完成了 项目打包脚本

--- Utils/trans_tool.py
+++ Utils/trans_tool.py
@@ -4,10 +4,9 @@
 # @Github : Orange-66
 # @PROJECT : LAPS 
 # @File : trans_tool.py
-# @Remark : 
+# @Remark : 转换工具
 # -----------------------------
 import os
-import os.path
 from tqdm import *
 from PIL import Image
 
@@ -130,15 +129,20 @@ def project_to_exe(project_name="LAPS"):
     spec_filename = "LAPS.spec"
     if os.path.exists(os.path.join(root_dir, spec_filename)):
         os.remove(os.path.join(root_dir, spec_filename))
+    else:
+        print(f'没有找到 [{os.path.join(root_dir, spec_filename)}] 文件或文件夹')
 
     # 删除workspace文件夹
     root_dir = "./EXE/LAPS/"
-    files = os.listdir(root_dir)
-    for filename in files:
-        if os.path.exists(os.path.join(root_dir, filename)):
-            os.remove(os.path.join(root_dir, filename))
     if os.path.exists(root_dir):
+        files = os.listdir(root_dir)
+        for filename in files:
+            if os.path.exists(os.path.join(root_dir, filename)):
+                os.remove(os.path.join(root_dir, filename))
+
         os.rmdir(root_dir)
+    else:
+        print(f'没有找到 [{root_dir}] 文件或文件夹')
 
 # 程序的主入口
 if __name__ == "__main__":
