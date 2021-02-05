6a9880d72e04fe3a23199a9745a25831cb8740f2
Orange-66 <qity_66@outlook.com>
2021-02-05 20:29:13 +0800

    - 更新了ui文件对应的py文件；

--- PyUI/ui_new_patient_single.py
+++ PyUI/ui_new_patient_single.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'ui_new_patient_single.ui'
+# Form implementation generated from reading ui file '../UI/ui_new_patient_single.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.2
 #
