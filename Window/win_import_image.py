1f74c6b42a55715181383c93e5ef72704368c712
Orange-66 <qity_66@outlook.com>
2021-03-08 22:17:31 +0800

    - 完善了单元测试；

---
+++ Window/win_import_image.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# @Time : 2021/3/4 12:09 上午
+# @Author : Qi Tian yue
+# @Github : Orange-66
+# @PROJECT : LAPS 
+# @File : win_import_image.py
+# @Remark : 
+# -----------------------------
+
+from PyQt5.QtWidgets import QWidget
+from PyUI.ui_import_image import Ui_Import_Image
+
+
+class Win_Import_Image(QWidget):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)  # 调用父类构造函数，创建窗体
+        self.__ui = Ui_Import_Image()  # 创建UI对象
+        self.__ui.setupUi(self)  # 构造UI界面
