5575225b3453a7f0bc7cbcbdbcccda7ccac01358
Orange-66 <qity_66@outlook.com>
2021-03-29 13:02:47 +0800

    - 修正了测试model的保存路径；

--- Utils/model_lap/test_main.py
+++ Utils/model_lap/test_main.py
@@ -1,8 +1,8 @@
 import function as f
 import os
 
-unpath = "..\\..\\Resource\\Images\\Button\\Temp\\uncropped_image.jpg"
-result, path = f.process_original_image(unpath)
+unpath = "../../Database/Temp/temp_painting.png"
+result, tau, path = f.process_original_image(unpath)
 
 print(result)
 print(path)
