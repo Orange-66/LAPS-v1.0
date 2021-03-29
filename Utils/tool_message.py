843fd4734167d177d622916d16e5c0c4dcd6abf0
Orange-66 <qity_66@outlook.com>
2021-03-30 02:57:53 +0800

    - 添加图片素材； - 更正了目录结构；

---
+++ Utils/tool_message.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Time : 2021/3/22 23:38
+# @Author : Qi Tian yue
+# @Github : Orange-66
+# @Project : LAPS
+# @File : tool_message.py
+# @Remark : 
+# -----------------------------
